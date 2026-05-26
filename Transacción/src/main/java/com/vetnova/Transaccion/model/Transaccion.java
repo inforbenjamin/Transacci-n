3ab932bc8a76c8f7f32646e2a8b395a51ec4251f
inforbenjamin <be.mellaf@duocuc.cl>
2026-05-26 13:52:54 -0400

    suviendo Kodigo

--- Transacción/src/main/java/com/vetnova/Transaccion/model/Transaccion.java
+++ Transacción/src/main/java/com/vetnova/Transaccion/model/Transaccion.java
@@ -2,22 +2,33 @@ package com.vetnova.Transaccion.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "transacciones")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transaccion {
     
- 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+     @Column(nullable = false)
     private LocalDate fecha;
+ 
+    @Column(nullable = false)
     private double total;
+ 
+    @Column(nullable = false, length = 50)
     private String estado;
 
 }
